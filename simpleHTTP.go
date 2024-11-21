88603dd143483696732bd1af5b0da31e0708fb2e
biao huang <726730581@qq.com>
2024-11-21 15:33:52 +0800

    Update simpleHTTP.go

--- simpleHTTP.go
+++ simpleHTTP.go
@@ -31,7 +31,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
 	}
 	fmt.Fprintf(w, "\n===> local IP: %q\n\n",GetOutboundIP())
-	fmt.Fprintf(w, "\n===> local IP: %q\n\n",GetOutboundIP())
 	fmt.Fprintf(w, "Env message = %s\n", os.Getenv("message"))
 }
 
